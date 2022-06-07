a4f075e02720013b52afd7f470070954ab5e3651
shamillov <shamillov@bk.ru>
2022-06-07 17:40:40 +0300

    add banner component

---
+++ app/src/main/java/com/shamilov/core/components/Banner.kt
@@ -0,0 +1,44 @@
+package com.shamilov.core.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.shamilov.core.ui.theme.Dimens
+
+data class BannerViewData(
+    val image: String,
+    val bannerHeight: Dp
+)
+
+@Composable
+fun BannerComponent(data: BannerViewData, modifier: Modifier = Modifier, onClick: () -> Unit) {
+    val image = data.image
+    val bannerHeight = data.bannerHeight
+
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(Dimens.cornerRadius))
+            .fillMaxWidth()
+            .height(bannerHeight)
+            .clickable { onClick() }
+    ) {
+        AsyncImage(model = image, contentDescription = null)
+    }
+}
+
+@Composable
+@Preview
+fun BannerComponentPreview() {
+    BannerComponent(data = BannerViewData(image = "", 100.dp)) {
+
+    }
+}
--- app/src/main/java/com/shamilov/core/ui/theme/Dimens.kt
+++ app/src/main/java/com/shamilov/core/ui/theme/Dimens.kt
@@ -1,9 +1,12 @@
 package com.shamilov.core.ui.theme
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 object Dimens {
     val largeText = 32.sp
     val mediumText = 22.sp
     val smallText = 15.sp
+
+    val cornerRadius = 20.dp
 }
